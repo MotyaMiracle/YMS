f867a9905ae81696e0351308f1b5bf1b580b8d65
Даниил Коурин <kourin.daniil@gmail.com>
2023-02-16 16:04:53 +0300

    Реализовал обновление информации пользователей

--- Yard_Management_System/Controllers/UsersController.cs
+++ Yard_Management_System/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Yard_Management_System.Entity;
 
 namespace Yard_Management_System.Controllers
 {
@@ -21,6 +22,41 @@ namespace Yard_Management_System.Controllers
         {
             var users = await db.Users.Include(u => u.Role).ToListAsync(token);
             return Ok(users);
+        }
+
+        [HttpPut("update")]
+        public async Task<ActionResult<User>> Edit(UpdateUser updateUser)
+        {
+            if (updateUser != null)
+            {
+                User? user = await db.Users.FirstOrDefaultAsync(p => p.Login == updateUser.Login);
+                if (user != null)
+                {
+                    if (updateUser.NewLogin != "string") 
+                    {
+                        user.Login = updateUser.NewLogin;
+                    }
+                    if (updateUser.NewPhoneNumber != "string")
+                    {
+                        user.PhoneNumber = updateUser.NewPhoneNumber;
+                    }
+                    if (updateUser.NewEmail != "string")
+                    {
+                        user.Email = updateUser.NewEmail;
+                    }
+                    if (updateUser.NewPassword != "string")
+                    {
+                        user.Password = updateUser.NewPassword;
+                        user.PasswordHash = Authorization.GetHash(user.Password);
+                    }
+                    
+                    db.Users.Update(user);
+                    await db.SaveChangesAsync();
+                    return Ok(user);
+                }
+                    
+            }
+            return NotFound();
         }
     }
 }
---
+++ Yard_Management_System/Entity/UpdateUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yard_Management_System.Entity
+{
+    public class UpdateUser
+    {
+        [Required]
+        public string Login { get; set; }
+        public string NewLogin { get; set; }
+        public string NewPhoneNumber { get; set; }
+        public string NewEmail { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
