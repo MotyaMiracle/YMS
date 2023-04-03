b53167e78d7b1d9f45d39caf4cf8b363a3442cd8
MotyaMiracle <motyamiracle@gmail.com>
2023-04-03 19:20:39 +0300

    fix conflicts

--- Database/Migrations/20230315192707_AddTimeslots.cs
+++ Database/Migrations/20230315192707_AddTimeslots.cs
@@ -11,19 +11,6 @@ namespace YardManagementSystem.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<Guid>(
-                name: "GateId",
-                table: "Trips",
-                type: "uuid",
-                nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
-
-            migrationBuilder.AddColumn<int>(
-                name: "PalletsCount",
-                table: "Trips",
-                type: "integer",
-                nullable: false,
-                defaultValue: 0);
 
             migrationBuilder.AddColumn<Guid>(
                 name: "TimeslotId",
@@ -39,17 +26,14 @@ namespace YardManagementSystem.Migrations
                     Id = table.Column<Guid>(type: "uuid", nullable: false),
                     Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
                     From = table.Column<string>(type: "text", nullable: false),
-                    To = table.Column<string>(type: "text", nullable: false)
+                    To = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Timeslots", x => x.Id);
                 });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Trips_GateId",
-                table: "Trips",
-                column: "GateId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Trips_TimeslotId",
@@ -63,21 +47,11 @@ namespace YardManagementSystem.Migrations
             migrationBuilder.DropTable(
                 name: "Timeslots");
 
-            migrationBuilder.DropIndex(
-                name: "IX_Trips_GateId",
-                table: "Trips");
 
             migrationBuilder.DropIndex(
                 name: "IX_Trips_TimeslotId",
                 table: "Trips");
 
-            migrationBuilder.DropColumn(
-                name: "GateId",
-                table: "Trips");
-
-            migrationBuilder.DropColumn(
-                name: "PalletsCount",
-                table: "Trips");
 
             migrationBuilder.DropColumn(
                 name: "TimeslotId",
