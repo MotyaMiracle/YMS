9e3d1b1467493e2f695e1194f505debd005d01af
MotyaMiracle <motyamiracle@gmail.com>
2023-04-24 15:11:28 +0300

    Check GateService, TripService

--- Domain/Entity/Trip.cs
+++ Domain/Entity/Trip.cs
@@ -26,5 +26,6 @@ namespace Domain.Entity
         public Gate Gate { get; set; }
         public string Number { get; set; }
         public int PalletsCount { get; set; }
+        public byte[] QRCode { get; set; }
     }
 }
